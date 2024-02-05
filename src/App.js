31bbf6e7bb1a7b37ffcdc210400b25cf0ca261f1
CabsT <133862761+CabsT@users.noreply.github.com>
2024-02-05 17:23:14 +0200

    Created a react application with necessary packages.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Dictionary from "./Dictionary";
+
+import "./App.css";
+
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="App">
+        <main>
+          <Dictionary />
+      
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DisplayData.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DisplayData(props) {
+  return <div>{props.keyword}</div>;
+}
